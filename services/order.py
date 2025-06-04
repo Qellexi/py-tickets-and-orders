3d15c55376f0f95f37e9abc4453fe9c81bcbdcc6
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-06-05 01:10:45 +0300

    Create order.py

---
+++ services/order.py
@@ -0,0 +1,22 @@
+from django.db import transaction
+from django.db.models import QuerySet
+
+from db.models import Order, User
+
+def create_order(
+  tickets: list[dict],
+  username: str,
+  date: str = None
+) -> None:
+  with transaction.atomic():
+    user = User.objects.get(username=username)
+    add_fields = {"user": user}
+    if date:
+      add_fields["created_at"] = date
+    order = Order.objects.create(**add_fields)
+  
+    for ticket in tickets:
+      Ticket.objects.create(order=order, **ticket)
+
+    return order
+    
