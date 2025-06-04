65662db3b9999bb1a3506593b3baa37045ad01ad
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-06-05 01:31:45 +0300

    Update movie_session.py

--- services/movie_session.py
+++ services/movie_session.py
@@ -46,4 +46,4 @@ def delete_movie_session_by_id(session_id: int) -> None:
 
 def get_taken_seats(movie_session_id: int) -> list[dict]:
     moviesession = MovieSession.objects.get(id=movie_session_id)
-    return [{"row": ticket.row, "seat": ticket.seat} for ticket in moviesession.tickets.all()]
+    return [{"row": ticket.row, "seat": ticket.seat} for ticket in moviesession.ticket_set.all()]
